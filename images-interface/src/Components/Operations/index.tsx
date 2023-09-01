4f1eb7d32cc50d357f6409889cf2f65aa5b206d7
josedhonatas <dhonatas@academico.ufs.br>
2023-08-31 22:38:17 -0300

    k

--- images-interface/src/Components/Operations/index.tsx
+++ images-interface/src/Components/Operations/index.tsx
@@ -1,50 +1,64 @@
 import React, { useEffect, useState } from 'react';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { Container, Grid, Box, Paper, MenuItem, Button } from '@mui/material';
-import axios from 'axios'
+import Select from '@mui/material/Select';
+import { Container, Grid, Paper, MenuItem, Button } from '@mui/material';
+import axios from 'axios';
+
+interface ImageData {
+  image_id: number;
+  image_url: string;
+}
 
 const Operations = () => {
-    const [images, setImages] = useState([])
+  const [images, setImages] = useState<ImageData[]>([]);
+  const [selectedImage, setSelectedImage] = useState("https://image-api.josedhonatas.ninja/images/1");
+  const [listimages, setListimages] = useState(['1', '2', '3', '4']);
+  const [filterimages, setFilterimages] = useState(['negative', 'thresh', 'gray', 'histeq', 'blur'])
 
-    useEffect(()=> {
-        getImages();
-    }, []);
+  useEffect(() => {
+    getImages();
+  }, []);
 
-    const getImages = () => {
-        axios
-        .get("https://image-api.josedhonatas.ninja/images/all")
-        .then((res) => setImages(res.data))
-        .catch((err) => console.log(err))
-    }
-    console.log(images)
-    return (
-        <Container>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <Paper>
-                {/* Exiba a imagem original aqui */}
-                <img  alt="Original" />
-              </Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper>
-                {/* Exiba a imagem filtrada aqui */}
-                <img  alt="Filtrada" />
-              </Paper>
-            </Grid>
-            <Grid item xs={12}>
-              <Select
-              >
-                {/* Opções de filtro no dropdown */}
-                <MenuItem value="">Nenhum</MenuItem>
-                <MenuItem value="filtro1">Filtro 1</MenuItem>
-                <MenuItem value="filtro2">Filtro 2</MenuItem>
-              </Select>
-              <Button variant="contained" >Aplicar Filtro</Button>
-            </Grid>
+  const getImages = () => {
+    axios
+      .get("https://image-api.josedhonatas.ninja/images/all")
+      .then((res) => {
+        setImages(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+
+  return (
+    <Container>
+      <Select label="Image">
+        {listimages.map((item, i) => (
+          <MenuItem value={item}>{item}</MenuItem>
+        ))}
+      </Select>
+      <Select label="Image">
+        {filterimages.map((item, i) => (
+          <MenuItem value={item}>{item}</MenuItem>
+        ))}
+      </Select>
+      {selectedImage && (
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Paper>
+              <img src={selectedImage} alt="Original" style={{ width: '100%' }} />
+            </Paper>
           </Grid>
-        </Container>
-      );
-    }
+          <Grid item xs={6}>
+            <Paper>
+              <img alt="Filtrada" />
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained">Aplicar Filtro</Button>
+          </Grid>
+        </Grid>
+      )}
+    </Container>
+  );
+};
 
 export default Operations;
