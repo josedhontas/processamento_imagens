d449b4c98343e5baf252bf2fca177e57cdc28f86
josedhonatas <dhonatas@academico.ufs.br>
2023-09-02 23:49:58 -0300

    quase

--- images-interface/src/Components/Theme/index.tsx
+++ images-interface/src/Components/Theme/index.tsx
@@ -2,11 +2,14 @@ import { createTheme } from '@mui/material/styles';
 
 export const Theme = createTheme({
   palette: {
+    mode: "light",
     primary: {
       main: '#28A745', // verde
     },
     secondary: {
       main: '#28A745', // Verde
     },
-  },
+  }, 
+  
+
 });
