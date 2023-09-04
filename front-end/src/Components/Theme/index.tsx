b9c2788fb0c6a74b7139d4332c03cb0b7bcae37b
josedhonatas <dhonatas@academico.ufs.br>
2023-09-03 23:02:23 -0300

    name

--- front-end/src/Components/Theme/index.tsx
+++ front-end/src/Components/Theme/index.tsx
@@ -2,14 +2,14 @@ import { createTheme } from '@mui/material/styles';
 
 export const Theme = createTheme({
   palette: {
-    mode: "light",
+    background: {
+      default: 'linear-gradient(to right bottom, #430089, #82ffa1)'
+    },
     primary: {
       main: '#28A745', // verde
     },
     secondary: {
       main: '#28A745', // Verde
     },
-  }, 
-  
-
+  },
 });
