4ab25f7a5ffafd0e2e0135140553020c298cfab2
josedhonatas <dhonatas@academico.ufs.br>
2023-10-17 23:54:57 -0300

    testando

--- front-end/src/Components/Operations/index.tsx
+++ front-end/src/Components/Operations/index.tsx
@@ -3,6 +3,9 @@ import Select from '@mui/material/Select';
 import { Container, Grid, Paper, MenuItem, Button, FormControl, InputLabel, Typography } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 const Operations = () => {
   const [selectedImage, setSelectedImage] = useState('');
@@ -11,7 +14,7 @@ const Operations = () => {
   const [filter, setFilter] = useState('')
   const [loading1, setLoading1] = useState(false)
   const [loading, setLoading] = useState(false);
-  const [filters, setFilters] = useState(['negative', 'thresh', 'gray', 'histeq','contrast'])
+  const [filters, setFilters] = useState(['negative', 'thresh', 'gray', 'histeq', 'contrast'])
   const [filteredImage, setFilteredImage] = useState('')
 
   useEffect(() => {
@@ -73,70 +76,83 @@ const Operations = () => {
   }, [imageName]);
 
   return (
-    <Container>
-      <br></br>
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={4}>
-          <FormControl fullWidth disabled={loading}>
-            <InputLabel>Image</InputLabel>
-            <Select
-              value={imageName}
-              onChange={e => setimageName(e.target.value)}
-              label="Image">
-              {listimages.map((item, i) => (
-                <MenuItem value={item} key={i}>
-                  {item}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+    <>
+      <Container>
+        <br></br>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={4}>
+            <FormControl fullWidth disabled={loading}>
+              <InputLabel>Image</InputLabel>
+              <Select
+                value={imageName}
+                onChange={e => setimageName(e.target.value)}
+                label="Image">
+                {listimages.map((item, i) => (
+                  <MenuItem value={item} key={i}>
+                    {item}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl fullWidth disabled={loading}>
+              <InputLabel>Filter</InputLabel>
+              <Select value={filter} onChange={e => setFilter(e.target.value)} label="filter">
+                {filters.map((item, i) => (
+                  <MenuItem value={item} key={i}>
+                    {item}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl fullWidth>
+              <Button variant="contained" onClick={applyFilter} size="large" >Apply Filter</Button>
+            </FormControl>
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <FormControl fullWidth disabled={loading}>
-            <InputLabel>Filter</InputLabel>
-            <Select value={filter} onChange={e => setFilter(e.target.value)} label="filter">
-              {filters.map((item, i) => (
-                <MenuItem value={item} key={i}>
-                  {item}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={4}>
-          <FormControl fullWidth>
-            <Button variant="contained" onClick={applyFilter} size="large" >Apply Filter</Button>
-          </FormControl>
-        </Grid>
-      </Grid>
-      <br></br>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={6}>
-          <Typography variant="h6" align="center"> Original</Typography>
-          <Paper elevation={3}>
+        <br></br>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={6}>
+            <Typography variant="h6" align="center"> Original</Typography>
             {loading1 ? (
               <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px' }}>
                 <CircularProgress />
               </div>
             ) : (
-              <img src={selectedImage} alt="Original" style={{ width: '80%' }} />
+              <img src={selectedImage} alt="Original" style={{ width: '75%' }} />
             )}
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={6}>
-          <Typography variant="h6" align='center'>Filtered</Typography>
-          <Paper elevation={3}>
+          </Grid>
+          
+          <Grid item xs={12} sm={6} md={6}>
+            <Typography variant="h6" align='center'>Filtered</Typography>
             {loading ? (
               <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px' }}>
                 <CircularProgress />
               </div>
             ) : (
-              <img src={filteredImage} style={{ width: '80%' }} alt="Filtered" />
+              <img src={filteredImage} style={{ width: '75%' }} alt="Filtered" />
             )}
-          </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+        <List>
+          <ListItem>
+            <ListItemText primary={`Name: ${imageName}`} />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary={`Channels: ${imageName}`} />
+          </ListItem>
+          <Divider/>
+          <ListItem>
+            <ListItemText primary={`Size: ${imageName}`} />
+          </ListItem>
+        </List>
+      </Container>
+    </>
+
   );
 };
 
